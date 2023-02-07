77c922007a6651e1db1bebf34608749186989827
Matias Aratano <matiasaratano@gmail.com>
2023-02-07 08:52:59 -0300

    mybatis runner implemented

--- computer-repair-service/src/main/java/com/solvd/app/mybatis/MyBatisRunner.java
+++ computer-repair-service/src/main/java/com/solvd/app/mybatis/MyBatisRunner.java
@@ -1,5 +1,9 @@
 package com.solvd.app.mybatis;
 
+import com.solvd.app.models.Customer;
+import com.solvd.app.models.Device;
+import com.solvd.app.service.mybatisimpl.MyBatisCustomerService;
+import com.solvd.app.service.mybatisimpl.MyBatisDeviceService;
 import com.solvd.app.service.mybatisimpl.MyBatisRepairService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +15,7 @@ public class MyBatisRunner {
     private final static Logger LOGGER = LogManager.getLogger(MyBatisRunner.class);
 
     public static void main(String[] args) throws SQLException {
-     /*   MyBatisCustomerService customerService = new MyBatisCustomerService();
+        MyBatisCustomerService customerService = new MyBatisCustomerService();
         LOGGER.info(customerService.getEntityById(3));
         Customer c = new Customer("123", "123", "123", "123");
         customerService.createEntity(c);
@@ -20,7 +24,7 @@ public class MyBatisRunner {
 
         MyBatisDeviceService deviceService = new MyBatisDeviceService();
         Device d = new Device("test", "test");
-        deviceService.createEntity(d);*/
+        deviceService.createEntity(d);
 
         MyBatisRepairService rs = new MyBatisRepairService();
         LOGGER.info(rs.getRepairById(9));
